b41b5a64f0afd45d3d327d25da8a4b867a4c4770
Nigel Cleland <nigel.cleland@gmail.com>
2013-05-09 20:52:21 +1200

    Bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='masks',
-    version='0.1.1',
+    version='0.1.2',
     author='Nigel Cleland',
     author_email='nigel.cleland@gmail.com',
     packages=['masks'],
